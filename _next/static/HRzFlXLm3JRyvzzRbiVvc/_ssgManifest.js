dd254faaf9e11e396e68c6c650949850b9050bb7
0702sjw-wq <0702sjw-wq@users.noreply.github.com>
2026-07-07 14:22:58 +0900

    Fix image paths for basePath (custom loader)

---
+++ _next/static/HRzFlXLm3JRyvzzRbiVvc/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fbrewery","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fproducts","\u002F[locale]\u002Fstory"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
